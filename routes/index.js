829782502c24e5c9359955f158e25106698f798b
junjie.sheng <junjie.sheng@cootek.cn>
2018-01-24 10:35:38 +0800

    add logs dir

--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,18 @@ const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   transports: [
-    //
-    // - Write to all logs with level `info` and below to `combined.log` 
-    // - Write all logs error (and below) to `error.log`.
-    //
     new winston.transports.File({ filename: '../../logs/test.log', level: 'error' }),
     new winston.transports.File({ filename: '../..logs/test.log' })
   ]
 });
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
 /* GET home page. */
 router.get('/', function (req, res, next) {
+	logger.log('info','test');
     res.sendfile(path.join(__dirname,'index.html'));
 });
 router.post('/test', passport.authenticate('local'), function(req, res) {
